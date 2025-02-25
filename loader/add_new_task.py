3e0c546713160d1b0417b5eb911fc7b1d173e48d
Junsu1115 <djkjs1115@gmail.com>
2025-02-25 09:09:43 +0900

    클래스 생성 후 path설정함

--- loader/add_new_task.py
+++ loader/add_new_task.py
@@ -3,28 +3,33 @@
 from pxr import Usd, Sdf, Kind
 import os
 
-def create_asset_root_stage(asset_name, dept, payload=False):
-    
-    add_path = f"/home/rapa/usd_test"
-    os.makedirs(add_path, exist_ok=True)  # 폴더가 없으면 생성
+class Create_task:
+    def __init__(self):
+        self.local_path = '/nas/eval/show'
 
-    root_stage_path = os.path.join(add_path, f"{asset_name}.usda")
-    asset_usd_name = f"{asset_name}_{dept}_v001.usd"
-    asset_usd_path = f"./{asset_usd_name}"
-    asset_root_stage = Usd.Stage.CreateNew(root_stage_path)
+    def create_asset_root_stage(self, project_name , asset_name, asset_type, dept, payload=False):
 
-    prim_type = input("타입을 선택해주세요: (Xform/Scope/Mesh/Material): ").strip()
-    if prim_type not in ["Xform", "Scope", "Mesh", "Material"]:
-        print("타입이 존재하지 않습니다. 기본은 Xform입니다.")
-        prim_type = "Xform"
+        assets_path = f"{self.local_path}/{project_name}/assets/{asset_type}/{asset_name}"
+        os.makedirs(assets_path, exist_ok=True)
 
-    root_prim = asset_root_stage.DefinePrim(f"/{asset_name}", prim_type)
+        root_stage_path = os.path.join(assets_path, f"{asset_name}.usda")
+        asset_usd_name = f"{asset_name}_{dept}_v001.usd"
+        asset_usd_path = f"./{asset_usd_name}"
+        asset_root_stage = Usd.Stage.CreateNew(root_stage_path)
 
-    if payload:
-        root_prim.GetPayloads().AddPayload(asset_usd_path)
-    else:
-        root_prim.GetReferences().AddReference(asset_usd_path)
+        prim_type = input("타입을 선택해주세요: (Xform/Scope/Mesh/Material): ").strip()
+        if prim_type not in ["Xform", "Scope", "Mesh", "Material"]:
+            print("타입이 존재하지 않습니다. 기본은 Xform입니다.")
+            prim_type = "Xform"
 
-    asset_root_stage.GetRootLayer().Save()
-create_asset_root_stage("junsu", "model", payload=True)
-create_asset_root_stage("jsw", "layout", payload=False)
+        root_prim = asset_root_stage.DefinePrim(f"/{asset_name}", prim_type)
+
+        if payload:
+            root_prim.GetPayloads().AddPayload(asset_usd_path)
+        else:
+            root_prim.GetReferences().AddReference(asset_usd_path)
+
+        asset_root_stage.GetRootLayer().Save()
+
+task = Create_task()
+task.create_asset_root_stage("project1", "junsu", "character", "model", payload=True)
